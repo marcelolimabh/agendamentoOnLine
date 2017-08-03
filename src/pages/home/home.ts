76f71aeba413bd1a322dc85f490c7b0e170707b0
marcelolimabh <marcelolimabh@gmail.com>
2017-08-02 23:37:48 -0300

    Commit inicial - Com listagem de veiculos utilizando uma api rest

---
+++ src/pages/home/home.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage implements OnInit {
+
+
+  public carros;
+
+  constructor(public navCtrl: NavController, private _http: Http, private loadCtrl: LoadingController, private _alertCtrl: AlertController) {
+
+
+
+
+  }
+
+  ngOnInit(): void {
+    let loader = this.loadCtrl.create({
+      content: 'Buscando novos carros...'
+    });
+    loader.present();
+    this._http
+      .get('https://aluracar.herokuapp.com')
+      .map(res => res.json())
+      .toPromise().then(carros => {
+        this.carros = carros;
+        loader.dismiss();
+      })
+      .catch(err => {
+        loader.dismiss();
+        console.log(err);
+        this._alertCtrl.create({
+          title: 'Falha na Conexão!',
+          subTitle: 'Ocorreu um problema ao tentar conectar no servidor. Favor tentar mais tarde!',
+          buttons: [{ text: 'Fechar' }]
+        }).present();
+      });
+
+  }
+
+
+
+}
