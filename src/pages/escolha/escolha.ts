db2073c5cf33ae84119a66f932a68e53260c4e04
marcelolimabh <marcelolimabh@gmail.com>
2017-08-03 20:43:29 -0300

    ajustes visuais na tela de escolha

--- src/pages/escolha/escolha.ts
+++ src/pages/escolha/escolha.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core'
+import { NavParams } from 'ionic-angular'
 
 @Component({
     templateUrl: 'escolha.html'
 })
 export class EscolhaPage{
+
+    public carro;
+
+    constructor(public navParams: NavParams){
+        this.carro = this.navParams.get('carroSelecionado');
+        console.log('Escolha:' + this.carro.nome);
+    }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Http } from '@angular/http';
+import { EscolhaPage } from '../escolha/escolha';
 
 @Component({
   selector: 'page-home',
@@ -43,6 +44,7 @@ export class HomePage implements OnInit {
   }
 
   seleciona(carro){
+    this.navCtrl.push(EscolhaPage, {carroSelecionado: carro});
     console.log(carro.nome);
   }
 
