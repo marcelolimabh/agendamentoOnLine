1452a4f9343f09381c546e134164d159f7abc39f
marcelolimabh <marcelolimabh@gmail.com>
2017-08-04 23:50:34 -0300

    chamada servico de gravacao de agendameto

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Http } from '@angular/http';
 
 // importou o tipo Carro para tiparmos a propriedade carro que guarda um objeto do tipo Carro
 import { Carro } from '../../domain/carro/carro';
@@ -18,7 +19,9 @@ export class CadastroPage {
   public email: string;
   public data: string = new Date().toISOString();
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              private _http: Http) {
 
     this.carro = navParams.get('carro');
     this.precoTotal = navParams.get('precoTotal');
@@ -26,10 +29,15 @@ export class CadastroPage {
   }
 
   agenda(){
-    console.log(this.nome);
-    console.log(this.endereco);
-    console.log(this.email);
-    console.log(this.data);
+
+    //Metodo de gravacao da api do cliente é GET deveria ser post , o fornecedor já está arrumando para ser post na proxima versao.
+    
+
+    let api = `https://aluracar.herokuapp.com/salvarpedido?carro=${this.carro.nome}&nome=${this.nome}&preco=${this.precoTotal}&endereco=${this.endereco}&email=${this.email}&dataAgendamento=${this.data}`;
+     this._http.get(api)
+            .toPromise()
+            .then(() => alert('Sucesso'))
+            .catch(erro => alert('Falha'));
   }
 
 
