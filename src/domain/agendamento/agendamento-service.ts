9b86fc7303a37c202cdb1f99c3bd3688f28b9df7
marcelolimabh <marcelolimabh@gmail.com>
2017-08-06 19:51:10 -0300

    Implementando regra de reenvio de agendamento

--- src/domain/agendamento/agendamento-service.ts
+++ src/domain/agendamento/agendamento-service.ts
@@ -14,13 +14,18 @@ export class AgendamentoService {
 
     constructor(private _http: Http, private _dao: AgendamentoDao) { }
 
+    private _montaUri(agendamento){
+           return  `${URL}salvarpedido?carro=${agendamento.carro.nome}&nome=${agendamento.nome}&preco=${agendamento.valor}&endereco=${agendamento.endereco}&email=${agendamento.email}&dataAgendamento=${agendamento.data}`;
+
+    }
+
 
     agenda(agendamento: Agendamento) {
 
 
         //Metodo de gravacao da api do cliente é GET deveria ser post , o fornecedor já está arrumando para ser post na proxima versao.
 
-        let api = `${URL}salvarpedido?carro=${agendamento.carro.nome}&nome=${agendamento.nome}&preco=${agendamento.valor}&endereco=${agendamento.endereco}&email=${agendamento.email}&dataAgendamento=${agendamento.data}`;
+        let api = this._montaUri(agendamento);
 
         return this._dao
             .ehAgendamentoDuplicado(agendamento)
@@ -32,6 +37,14 @@ export class AgendamentoService {
                       .then(() => agendamento.confirmado);
             });
 
+    }
+
+    reagenda(agendamento: Agendamento){
+        let api = this._montaUri(agendamento);
+        return this._http.get(api).toPromise()
+        .then(()=> agendamento.confirmado = true, err => console.log(err))
+        .then(() => this._dao.salva(agendamento))
+        .then(()=> agendamento.confirmado); 
     }
 
 }
--- src/pages/agendamentos/agendamentos.ts
+++ src/pages/agendamentos/agendamentos.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
 import { AgendamentoDao } from '../../domain/agendamento/agendamento-dao';
 
 import { Agendamento } from '../../domain/agendamento/agendamento';
+import { AgendamentoService } from '../../domain/agendamento/agendamento-service';
 
 @Component({
   selector: 'page-agendamentos',
@@ -13,11 +14,34 @@ export class AgendamentosPage {
   public agendamentos: Agendamento[];
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    private _dao: AgendamentoDao
+    private _dao: AgendamentoDao,
+    private _service: AgendamentoService,
+    private _alertCtrl: AlertController
   ) {
     this._dao.listaTodos().then(agendamentos => this.agendamentos = agendamentos);
   }
 
 
+  reenvia(agendamento) {
+
+    this._service.reagenda(agendamento)
+      .then(confirmado => {
+
+        confirmado ?
+          this._alertCtrl.create({
+            title: 'Envio',
+            subTitle: 'Reenvio de agendamento realizado com sucesso!',
+            buttons: [{ text: 'Ok' }]
+
+          }).present()
+          :
+          this._alertCtrl.create({
+            title: 'Envio',
+            subTitle: 'Não foi possivel realizar o agendamento. Tente mais tarde.',
+            buttons: [{ text: 'Ok' }]
+          }).present();
+
+      });
+  }
 
 }
